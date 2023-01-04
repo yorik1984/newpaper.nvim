48418d8024d4a36aaf31ed35900dfcf678d9074c
yorik1984 <yorik1984@gmail.com>
2023-01-04 04:32:11 +0200

    Update yaml.lua

--- lua/newpaper/theme/filetypes/yaml.lua
+++ lua/newpaper/theme/filetypes/yaml.lua
@@ -11,7 +11,7 @@ function M.setup(configColors, configStyle)
     local newpaper = yamlSyn.colors
     local style    = yamlSyn.style
 
-    yamlSyn.loadSyntax = function ()
+    yamlSyn.loadSyntax = function()
         local syntax = {
             -- yamlTodo                     Todo
             -- yamlComment                  Comment
@@ -43,8 +43,8 @@ function M.setup(configColors, configStyle)
             -- yamlFlowMappingMerge         Special
 
             yamlMappingKeyStart          = { fg = newpaper.orange },
-            yamlFlowIndicator            = { fg = newpaper.tag_navy, style = style.o_style },
-            yamlKeyValueDelimiter        = { fg = newpaper.orange,   style = style.d_style },
+            yamlFlowIndicator            = { fg = newpaper.tag_navy, style = style.br_style },
+            yamlKeyValueDelimiter        = { fg = newpaper.persimona, style = style.d_style },
 
             yamlConstant                 = { fg = newpaper.darkengreen, style = style.k_style  },
 
@@ -62,19 +62,23 @@ function M.setup(configColors, configStyle)
         return syntax
     end
 
-    yamlSyn.loadTreeSitter = function ()
+    yamlSyn.loadTreeSitter = function()
 
         -- fallback to 0.7
         local treesitterOldKey = {
-            ["@field.yaml"]                 = "yamlTSField",
-            ["@punctuation.bracket.yaml"]   = "yamlTSPunctBracket",
-            ["@string.escape.yaml"]         = "yamlTSStringEscape",
+            ["@constant.builtin.yaml"]    = "yamlTSConstBuiltin",
+            ["@field.yaml"]               = "yamlTSField",
+            ["@punctuation.bracket.yaml"] = "yamlTSPunctBracket",
+            ["@punctuation.special.yaml"] = "yamlTSPunctSpecial",
+            ["@string.escape.yaml"]       = "yamlTSStringEscape",
         }
 
         local treesitter = {
-            ["@field.yaml"]                 = { fg = newpaper.ocean, style = style.f_style },
-            ["@punctuation.bracket.yaml"]   = { fg = newpaper.tag_navy, style = style.br_style },
-            ["@string.escape.yaml"]         = { fg = newpaper.magenta },
+            ["@constant.builtin.yaml"]    = { fg = newpaper.ocean, style = style.k_style },
+            ["@field.yaml"]               = { fg = newpaper.ocean, style = style.f_style, nocombine = true },
+            ["@punctuation.bracket.yaml"] = { fg = newpaper.tag_navy, style = style.br_style },
+            ["@punctuation.special.yaml"] = { fg = newpaper.magenta },
+            ["@string.escape.yaml"]       = { fg = newpaper.magenta },
         }
 
         -- fallback to 0.7
