5f30c6dd4b4ef7146b8444142f68ee3539176bfe
yorik1984 <yorik1984@gmail.com>
2023-11-21 20:17:22 +0200

    theme: update query `markdown`

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -7,6 +7,31 @@
   (fenced_code_block_delimiter)
 ] @text.literal
 
+;; Conceal backticks
+(fenced_code_block
+  (fenced_code_block_delimiter) @text.literal
+  (#set! conceal ""))
+(fenced_code_block
+  (info_string (language) @text.literal
+  (#set! conceal "")))
+
+;; Conceal bullet points
+([(list_marker_plus) (list_marker_star)]
+  @keyword
+  (#offset! @keyword 0 0 0 -1)
+  (#set! conceal "•"))
+([(list_marker_plus) (list_marker_star)]
+  @keyword
+  (#any-of? @keyword "+" "*")
+  (#set! conceal "•"))
+((list_marker_minus)
+  @keyword
+  (#offset! @keyword 0 0 0 -1)
+  (#set! conceal "—"))
+((list_marker_minus)
+  @keyword
+  (#eq? @keyword "-")
+  (#set! conceal "—"))
 [
   (link_destination)
 ] @text.underline
