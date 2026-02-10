08b036c33baedbef78f635a131ce1d4f21313109
yorik1984 <yorik1984@gmail.com>
2026-02-10 15:15:39 +0200

    theme: add `markdown-plus.nvim`

--- lua/newpaper/theme/ftplugins/init.lua
+++ lua/newpaper/theme/ftplugins/init.lua
@@ -3,13 +3,14 @@ local M = {}
 local folder = "newpaper.theme.ftplugins."
 
 local files = {
-  "vim-jinja2-syntax",
-  "vim-lua",
-  "vim-markdown",
-  "vim-pandoc-syntax",
-  "vim-rbs",
-  "vim-ruby",
-  "vimtex",
+    "markdown-plus",
+    "vim-jinja2-syntax",
+    "vim-lua",
+    "vim-markdown",
+    "vim-pandoc-syntax",
+    "vim-rbs",
+    "vim-ruby",
+    "vimtex",
 }
 
 for _, value in ipairs(files) do
