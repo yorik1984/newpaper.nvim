39295aaf6001e137c7d5e89895596de4ec3a0d8d
yorik1984 <yorik1984@gmail.com>
2026-03-30 12:34:35 +0300

    Update nvim-tree.lua

--- lua/newpaper/theme/plugins/nvim-tree.lua
+++ lua/newpaper/theme/plugins/nvim-tree.lua
@@ -5,40 +5,126 @@ function M.setup(configColors, configStyle)
     local style    = configStyle
 
     return {
+        -- Standard
         NvimTreeNormal            = { fg = newpaper.nvimtree_fg, bg = newpaper.nvimtree_bg },
+        -- NvimTreeNormalFloat         NormalFloat
+        -- NvimTreeNormalNC            NvimTreeNormal
+
         NvimTreeLineNr            = { bg = newpaper.nvimtree_bg },
+        -- NvimTreeWinSeparator        WinSeparator
+        -- NvimTreeEndOfBuffer         EndOfBuffer
+        -- NvimTreePopup               Normal
+        NvimTreeSignColumn        = { bg = newpaper.nvimtree_bg },
+
+        NvimTreeCursorColumn      = { bg = newpaper.nvimtree_bg },
         NvimTreeCursorLine        = { bg = newpaper.aqua },
         NvimTreeCursorLineNr      = { bg = newpaper.nvimtree_bg },
-        NvimTreeCursorColumn      = { bg = newpaper.nvimtree_bg },
-        NvimTreeSignColumn        = { bg = newpaper.nvimtree_bg },
-        NvimTreeSymlink           = { fg = newpaper.accent, style = style.links },
-        NvimTreeFolderName        = { fg = newpaper.teal },
-        NvimTreeFolderIcon        = { fg = newpaper.teal },
-        NvimTreeOpenedFolderName  = { fg = newpaper.teal, style = style.b_bold },
-        NvimTreeRootFolder        = { fg = newpaper.titles, style = style.b_bold },
-        NvimTreeEmptyFolderName   = { fg = newpaper.disabled },
-        NvimTreeIndentMarker      = { fg = newpaper.teal },
-        NvimTreeGitDirty          = { fg = newpaper.git_modified },
-        NvimTreeGitNew            = { fg = newpaper.git_untracked },
-        NvimTreeGitStaged         = { fg = newpaper.git_added },
-        NvimTreeGitDeleted        = { fg = newpaper.git_removed },
-        NvimTreeGitMerge          = { fg = newpaper.git_merged },
-        NvimTreeGitRenamed        = { fg = newpaper.git_modified },
-        NvimTreeLicenseIcon       = { fg = newpaper.navy },
-        NvimTreeGitignoreIcon     = { fg = newpaper.git_fg },
-        NvimTreeOpenedFile        = { style = style.b_bold },
-        NvimTreeImageFile         = { fg = newpaper.orange },
-        NvimTreeMarkdownFile      = { fg = newpaper.strings },
+
+        -- NvimTreeStatusLine          StatusLine
+        -- NvimTreeStatusLineNC        StatusLineNC
+
+        -- File Text
         NvimTreeExecFile          = { fg = newpaper.texts },
-        NvimTreeSpecialFile       = { fg = newpaper.purple, style = style.underline },
+        NvimTreeImageFile         = { fg = newpaper.orange },
+        NvimTreeSpecialFile       = { fg = newpaper.purple },
+        NvimTreeSymlink           = { fg = newpaper.links, style = style.links },
+
+        -- Folder Text
+        NvimTreeRootFolder        = { fg = newpaper.titles, style = style.b_bold },
+        NvimTreeFolderName        = { fg = newpaper.teal },
+        NvimTreeEmptyFolderName   = { fg = newpaper.disabled },
+        NvimTreeOpenedFolderName  = { fg = newpaper.teal, style = style.b_bold },
+        NvimTreeSymlinkFolderName = { fg = newpaper.links, style = style.b_links },
+
+        -- File Icons
+        -- NvimTreeFileIcon            NvimTreeNormal
+        -- NvimTreeSymlinkIcon         NvimTreeNormal
+
+        -- Folder Icons
+        NvimTreeFolderIcon        = { fg = newpaper.teal },
+        -- NvimTreeOpenedFolderIcon    NvimTreeFolderIcon
+        -- NvimTreeClosedFolderIcon    NvimTreeFolderIcon
+        -- NvimTreeFolderArrowClosed   NvimTreeIndentMarker
+        -- NvimTreeFolderArrowOpen     NvimTreeIndentMarker
+
+        -- Indent
+        NvimTreeIndentMarker      = { fg = newpaper.teal },
+
+        -- Picker
+        NvimTreeWindowPicker      = { fg = newpaper.bg, bg = newpaper.accent, style = style.b_bold },
+
+        -- Live Filter
+        -- NvimTreeLiveFilterPrefix    PreProc
+        -- NvimTreeLiveFilterValue     ModeMsg
+
+        -- Clipboard
         NvimTreeCopiedHL          = { bg = newpaper.difftext },
         NvimTreeCutHL             = { bg = newpaper.diffdelete },
-        NvimTreeBookmark          = { fg = newpaper.darkgreen },
+
+        -- Bookmarks
+        NvimTreeBookmarkIcon      = { fg = newpaper.darkgreen },
         NvimTreeBookmarkHL        = { fg = newpaper.green, style = style.b_bold },
-        LspDiagnosticsError       = { fg = newpaper.error_fg },
-        LspDiagnosticsWarning     = { fg = newpaper.warn_fg },
-        LspDiagnosticsInformation = { fg = newpaper.info_fg },
-        LspDiagnosticsHint        = { fg = newpaper.hint_fg },
+
+        -- Modified
+        NvimTreeModifiedIcon      = { style = style.b_bold },
+        -- NvimTreeModifiedFileHL      NvimTreeModifiedIcon
+        -- NvimTreeModifiedFolderHL    NvimTreeModifiedIcon
+
+        -- Hidden
+        -- NvimTreeModifiedIcon        Conceal
+        -- NvimTreeModifiedFileHL      NvimTreeHiddenIcon
+        -- NvimTreeModifiedFolderHL    NvimTreeHiddenFileHL
+
+        -- Hidden Display
+        -- NvimTreeHiddenDisplay       Conceal
+
+        -- Opened
+        NvimTreeOpenedHL          = { style = style.b_bold },
+
+        -- Git Icon
+        NvimTreeGitDeletedIcon    = { fg = newpaper.git_removed },
+        NvimTreeGitDirtyIcon      = { fg = newpaper.git_modified },
+        NvimTreeGitIgnoredIcon    = { fg = newpaper.disabled },
+        NvimTreeGitMergeIcon      = { fg = newpaper.git_merged },
+        NvimTreeGitNewIcon        = { fg = newpaper.git_untracked },
+        NvimTreeGitRenamedIcon    = { fg = newpaper.git_modified },
+        NvimTreeGitStagedIcon     = { fg = newpaper.git_added },
+
+        -- Git File File Highlight
+        -- NvimTreeGitFileDeletedHL    NvimTreeGitDeletedIcon
+        -- NvimTreeGitFileDirtyHL      NvimTreeGitDirtyIcon
+        -- NvimTreeGitFileIgnoredHL    NvimTreeGitIgnoredIcon
+        -- NvimTreeGitFileMergeHL      NvimTreeGitMergeIcon
+        -- NvimTreeGitFileNewHL        NvimTreeGitNewIcon
+        -- NvimTreeGitFileRenamedHL    NvimTreeGitRenamedIcon
+        -- NvimTreeGitFileStagedHL     NvimTreeGitStagedIcon
+
+        -- Git Folder Folder Highlight
+        -- NvimTreeGitFolderDeletedHL  NvimTreeGitFileDeletedHL
+        -- NvimTreeGitFolderDirtyHL    NvimTreeGitFileDirtyHL
+        -- NvimTreeGitFolderIgnoredHL  NvimTreeGitFileIgnoredHL
+        -- NvimTreeGitFolderMergeHL    NvimTreeGitFileMergeHL
+        -- NvimTreeGitFolderNewHL      NvimTreeGitFileNewHL
+        -- NvimTreeGitFolderRenamedHL  NvimTreeGitFileRenamedHL
+        -- NvimTreeGitFolderStagedHL   NvimTreeGitFileStagedHL
+
+        -- Diagnostics Icon
+        -- NvimTreeDiagnosticErrorIcon         DiagnosticError
+        -- NvimTreeDiagnosticWarnIcon          DiagnosticWarn
+        -- NvimTreeDiagnosticInfoIcon          DiagnosticInfo
+        -- NvimTreeDiagnosticHintIcon          DiagnosticHint
+
+        -- Diagnostics File Highlight
+        -- NvimTreeDiagnosticErrorFileHL       DiagnosticUnderlineError
+        -- NvimTreeDiagnosticWarnFileHL        DiagnosticUnderlineWarn
+        -- NvimTreeDiagnosticInfoFileHL        DiagnosticUnderlineInfo
+        -- NvimTreeDiagnosticHintFileHL        DiagnosticUnderlineHint
+
+        -- Diagnostics Folder Highlight
+        -- NvimTreeDiagnosticErrorFolderHL     NvimTreeDiagnosticErrorFileHL
+        -- NvimTreeDiagnosticWarnFolderHL      NvimTreeDiagnosticWarnFileHL
+        -- NvimTreeDiagnosticInfoFolderHL      NvimTreeDiagnosticInfoFileHL
+        -- NvimTreeDiagnosticHintFolderHL      NvimTreeDiagnosticHintFileHL
     }
 end
 
