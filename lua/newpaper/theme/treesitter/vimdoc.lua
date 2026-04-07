0c4ae34ca894993f1c2e193d51d11afdeeb2b919
yorik1984 <yorik1984@gmail.com>
2026-04-07 14:38:10 +0300

    Update vimdoc.lua

--- lua/newpaper/theme/treesitter/vimdoc.lua
+++ lua/newpaper/theme/treesitter/vimdoc.lua
@@ -5,9 +5,10 @@ function M.setup(configColors, configStyle)
     local style    = configStyle
 
     return {
-        ["@label.vimdoc"]          = { fg = newpaper.tex_magenta },
-        ["@markup.link.vimdoc"]    = { fg = newpaper.links, style = style.underline },
-        ["@string.special.vimdoc"] = { fg = newpaper.keywords, nocombine = true },
+        ["@label.vimdoc"]             = { fg = newpaper.tex_magenta, nocombine = true },
+        ["@markup.link.vimdoc"]       = { fg = newpaper.links, style = style.underline, nocombine = true },
+        ["@string.special.vimdoc"]    = { fg = newpaper.keywords, nocombine = true },
+        ["@markup.link.label.vimdoc"] = { fg = newpaper.tex_maroon, style = style.b_bold, nocombine = true },
     }
 end
 
